432e4a806231fe3f828e4bbbaafcee45275bd184
ewelina <skibinska.ewelina@gmail.com>
2017-05-12 20:00:52 +0100

    Add .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+   entry: path.resolve(__dirname, 'src', 'index.jsx'),
+   output: {
+      path: path.resolve(__dirname, 'output'),
+      filename: 'bundle.js'
+   },
+   module: {
+      rules: [
+         {
+             test: /\.jsx/,
+             use: {
+                loader: 'babel-loader',
+                options: { presets: ['react', 'es2015'] }
+             }
+         },
+         {
+            test: /\.scss/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+         }
+      ]
+   }
+ };
